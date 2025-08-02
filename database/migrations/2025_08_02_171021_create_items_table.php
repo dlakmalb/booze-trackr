7c8186d93559afffec0ed800e7f7d30b5a1e844d
Dhananjaya Bulathsinghala <dhananjaya@fixico.com>
2025-08-03 00:10:44 +0530

    remove migration file modification

--- database/migrations/2025_08_02_171021_create_items_table.php
+++ database/migrations/2025_08_02_171021_create_items_table.php
@@ -13,9 +13,6 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('description')->nullable();
-            $table->decimal('price', 8, 2);
             $table->timestamps();
         });
     }
